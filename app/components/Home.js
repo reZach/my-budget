7d3090287ca43c4ebe646ad76b9327ed681d276a
Zachary Wenta <reZach@users.noreply.github.com>
2019-03-04 19:41:07 -0600

    first

---
+++ app/components/Home.js
@@ -0,0 +1,22 @@
+// @flow
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../constants/routes';
+import styles from './Home.css';
+import Date from "./Date";
+
+type Props = {};
+
+export default class Home extends Component<Props> {
+  props: Props;
+
+  render() {
+    return (
+      <div className={styles.container} data-tid="container">
+        <Date />
+        <h2>Home</h2>
+        <Link to={routes.COUNTER}>to Counter</Link>
+      </div>
+    );
+  }
+}
