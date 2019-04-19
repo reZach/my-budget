ffa1ad368f028d158a7cf552aab65ae9b301455a
Zachary Wenta <reZach@users.noreply.github.com>
2019-04-18 23:24:53 -0500

    getting that tickbox ready to import transactions

--- app/components/ImportBank/ImportBank.js
+++ app/components/ImportBank/ImportBank.js
@@ -7,21 +7,37 @@ export default class ImportBank extends Component<Props> {
 
     constructor(){
         super();
-        this.state = {
-            
-        }
+        
+        this.trimNote = this.trimNote.bind(this);
     }    
+
+    trimNote(note){
+        if (note.length > 9){
+            return note.substring(0, 8) + "...";
+        } else {
+            return note;
+        }
+    }
 
     render () {
         return (
-            <div className={`columns ${styles.importrow}`}>                    
+            <div className={`columns ${styles.importrow}`}>
+                <div className="column col-1">
+                    <input type="checkbox" value="import" defaultChecked={this.props.import} onClick={() => this.props.toggleImport(this.props.tempId)}></input>                    
+                </div>                    
                 <div className="column col-2">
                     {`${this.props.dateId}`}
                 </div>
                 <div className="column col-2">
                     ${this.props.amount}
                 </div>
-                <div className={`column col-8 ${styles.note}`}>{this.props.note}</div>
+                <div className="column col-2">
+                    {this.props.categoryName}
+                </div>
+                <div className="column col-2">
+                    {this.props.itemName}
+                </div>
+                <div className={`column col-3 ${styles.note}`}>{this.trimNote(this.props.note)}</div>
             </div>
         );
     }
